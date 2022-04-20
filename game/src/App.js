1b48dc4a251cf7ceb2fd253f616e8446e363369c
DavisDavisDavis <olizoooom@gmail.com>
2022-04-20 12:02:00 +0200

    broken canvas 😊

--- game/src/App.js
+++ game/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+
+React.useEffect(() => {
+  var canvas = document.getElementById("canvas");
+  var ctx = canvas.getContext("2d");
+  canvas.width = 400;
+  canvas.height = 400;
+}, []);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Spaaaaaaaaaaaaace</h1>
+      <canvas id="canvas"></canvas>
     </div>
   );
 }
