718720f7f1a6161df4369a0d7f639a553857cc85
DavisDavisDavis <olizoooom@gmail.com>
2022-04-27 12:04:08 +0200

    Redoing everything because NASA weather senors stopped working 2 years ago 🤬😡

--- game/src/App.js
+++ game/src/App.js
@@ -3,31 +3,31 @@ import React from "react";
 import News from "./components/News/index.js";
 
 function App() {
-  const [news, setNews] = React.useState("");
+  // const [news, setNews] = React.useState("");
 
-  // UseEffect will run on mount (when the component is ready) and everytime the inputText state changes
-  React.useEffect(() => {
-    fetch(
-      "https://api.nasa.gov/planetary/apod?api_key=dv45uJ17VkIm1YYqBJAa5HR81sqkabxl9RkNAKVb"
-    )
-      .then((res) => res.json())
-      .then((json) => {
-        setNews(json);
-      });
-  }, []); // Mapping useEffect function to change whenever the inputText state variable changes
+  // // UseEffect will run on mount (when the component is ready) and everytime the inputText state changes
+  // React.useEffect(() => {
+  //   fetch(
+  //     "https://api.nasa.gov/planetary/apod?api_key=dv45uJ17VkIm1YYqBJAa5HR81sqkabxl9RkNAKVb"
+  //   )
+  //     .then((res) => res.json())
+  //     .then((json) => {
+  //       setNews(json);
+  //     });
+  // }, []); // Mapping useEffect function to change whenever the inputText state variable changes
 
-  //console.log(news);
+  // //console.log(news);
 
-  React.useEffect(() => {
-    fetch(
-      "https://api.nasa.gov/insight_weather/?api_key=dv45uJ17VkIm1YYqBJAa5HR81sqkabxl9RkNAKVb&feedtype=json&ver=1.0"
-    )
-      .then((res) => res.json())
-      .then((json) => {
-        console.log(json.validity_checks[1199].Season);
-        //json.validity_checks[1199].AT.sol_hours_with_data[0]
-      });
-  }, []);
+  // React.useEffect(() => {
+  //   fetch(
+  //     "https://api.nasa.gov/insight_weather/?api_key=dv45uJ17VkIm1YYqBJAa5HR81sqkabxl9RkNAKVb&feedtype=json&ver=1.0"
+  //   )
+  //     .then((res) => res.json())
+  //     .then((json) => {
+  //       console.log(json.sol_keys[1199]);
+  //       //json.validity_checks[1199].AT.sol_hours_with_data[0]
+  //     });
+  // }, []);
 
   return (
     <div className="App">
