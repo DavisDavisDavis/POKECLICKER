9fdab6bcc3da97c34b66f7955911811e3de17d1e
DavisDavisDavis <olizoooom@gmail.com>
2022-04-27 10:35:48 +0200

    NASA api up and running 🚀

--- game/src/App.js
+++ game/src/App.js
@@ -1,17 +1,27 @@
 import "./App.css";
 import React from "react";
-
-React.useEffect(() => {
-  var canvas = document.getElementById("canvas");
-  var ctx = canvas.getContext("2d");
-  canvas.width = 400;
-  canvas.height = 400;
-}, []);
+import News from "./components/News/index.js";
 
 function App() {
+  const [news, setNews] = React.useState("");
+
+  // UseEffect will run on mount (when the component is ready) and everytime the inputText state changes
+  React.useEffect(() => {
+    fetch(
+      "https://api.nasa.gov/planetary/apod?api_key=dv45uJ17VkIm1YYqBJAa5HR81sqkabxl9RkNAKVb"
+    )
+      .then((res) => res.json())
+      .then((json) => {
+        setNews(json);
+      });
+  }, []); // Mapping useEffect function to change whenever the inputText state variable changes
+
+  console.log(news);
+
   return (
     <div className="App">
-      <h1>Spaaaaaaaaaaaaace</h1>
+      <h1>What is the weather like at Mars? 🪐</h1>
+      <News></News>
       <canvas id="canvas"></canvas>
     </div>
   );
---
+++ game/src/components/News/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const News = () => {
+  return (
+    <article>
+      <h2></h2>
+      <img />
+      <p></p>
+    </article>
+  );
+};
+
+export default News;
