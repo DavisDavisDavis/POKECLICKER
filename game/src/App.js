bb4f94b24d91f926d0f4fdec0e6e961d2bf74412
DavisDavisDavis <olizoooom@gmail.com>
2022-04-30 12:48:30 +0200

    Initial creature class form Pokemons with attacks 🌱

--- game/src/App.js
+++ game/src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 import React from "react";
 import News from "./components/News/index.js";
+import { Pokemon, Creature } from "./components/Pokemon/index.js";
+import Button from "./components/Button/index.js";
 
 function App() {
-  // const [news, setNews] = React.useState("");
+  const [news, setNews] = React.useState("");
 
   // // UseEffect will run on mount (when the component is ready) and everytime the inputText state changes
   // React.useEffect(() => {
@@ -15,25 +17,19 @@ function App() {
   //       setNews(json);
   //     });
   // }, []); // Mapping useEffect function to change whenever the inputText state variable changes
+  // <News title={news.title} img={news.url} text={news.explanation}></News>
 
-  // //console.log(news);
-
-  // React.useEffect(() => {
-  //   fetch(
-  //     "https://api.nasa.gov/insight_weather/?api_key=dv45uJ17VkIm1YYqBJAa5HR81sqkabxl9RkNAKVb&feedtype=json&ver=1.0"
-  //   )
-  //     .then((res) => res.json())
-  //     .then((json) => {
-  //       console.log(json.sol_keys[1199]);
-  //       //json.validity_checks[1199].AT.sol_hours_with_data[0]
-  //     });
-  // }, []);
+  const Piplup = new Creature("Piplup", 100, 20);
+  const Charmander = new Creature("Piplup", 50, 30);
+  console.log(Charmander.hp);
+  Piplup.attack(Charmander);
+  console.log(Charmander.hp);
 
   return (
     <div className="App">
-      <h1>What is the weather like at Mars? 🪐</h1>
-      <News title={news.title} img={news.url} text={news.explanation}></News>
-      <canvas id="canvas"></canvas>
+      <h1>Pokemon 🌱</h1>
+      <Pokemon image="https://assets.pokemon.com/assets/cms2/img/pokedex/full/393.png"></Pokemon>
+      <Button text="Attack"></Button>
     </div>
   );
 }
---
+++ game/src/components/Pokemon/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+export class Creature {
+  constructor(name, hp, power) {
+    this.name = name;
+    this.hp = hp;
+    this.power = power;
+  }
+
+  attack(enemy) {
+    enemy.hp -= this.power;
+  }
+}
+
+export const Pokemon = (props) => {
+  return (
+    <div>
+      <img src={props.image} />
+    </div>
+  );
+};
